a6d99fc5e65e1d796a7de27e8c30ac40ba3a105b
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-01-28 16:02:36 -0500

    Add ajax call that lumps the login form in the middle of the landing page when the login link is clicked. is ugly, but it works.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#login-link').on('click', function(event) {
+    console.log("you clicked log-in");
+    event.preventDefault();
+    var $target = $(event.target);
+
+    $.ajax({
+      type: 'GET',
+      url: $target.attr('href'),
+      dataType: 'html'
+    }).done(function(response){
+      // debugger
+      $target.closest('#content').children('#pop-in').html(response);
+    });
+
+  });
+
 });
