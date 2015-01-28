589206eb847c1f2c2f5c08c03be5aec6f7a5138c
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-01-28 18:56:56 -0500

    correct password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
+
+
   validates :name, {
     :presence => true,
     uniqueness: true
   }
 
-  validates :password, :presence => true
+  has_secure_password
+  validates_presence_of :password, :on => :create
 
   has_many :movies
 end
