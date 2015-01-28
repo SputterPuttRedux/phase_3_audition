29933b29fed502824471ac0a5e24f65c1a23c97d
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-01-28 09:43:00 -0500

    Create models and add columns to migrations

--- db/migrate/20150128093617_create_movies.rb
+++ db/migrate/20150128093617_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t|
+      t.string :name
+      t.text :description
+      t.integer :rating
+      t.references :user
+    end
   end
 end
--- db/migrate/20150128093625_create_categories.rb
+++ db/migrate/20150128093625_create_categories.rb
@@ -1,4 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def change
+    create_table :categories do |t|
+      t.references :movie
+      t.references :genre
+    end
   end
 end
