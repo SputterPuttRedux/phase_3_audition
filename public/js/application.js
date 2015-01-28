ee908ed2102caf4082f0d1121e0f067fcaccd580
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-01-28 18:40:58 -0500

    remove console log. correct type in parameter passed to jquery traversal method.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   $('#login-link').on('click', function(event) {
-    console.log("you clicked log-in");
     event.preventDefault();
     var $target = $(event.target);
 
@@ -10,13 +9,30 @@ $(document).ready(function() {
       url: $target.attr('href'),
       dataType: 'html'
     }).done(function(response){
-      $resp = $(response).children('#auth-container')
+      $resp = $(response).children('.auth-container')
       $target.closest('#content').children('#pop-in').html($resp);
       debugger
     });
 
     $('#content').css("backgroundColor","#888877")
 
+  });
+
+  $('#signup-link').on('click', function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+
+    $.ajax({
+      type: 'GET',
+      url: $target.attr('href'),
+      dataType: 'html'
+    }).done(function(response){
+      $resp = $(response).children('.auth-container')
+      $target.closest('#content').children('#pop-in').html($resp);
+      debugger
+    });
+
+    $('#content').css("backgroundColor","#888877")
 
   });
 
