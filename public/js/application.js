92e9c53b715b8ba5e004bbceec73c3181726acae
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-01-28 20:37:09 -0500

    Add AJAX call to delete an entry on a user's dashboard page

--- public/js/application.js
+++ public/js/application.js
@@ -9,13 +9,12 @@ $(document).ready(function() {
       url: $target.attr('href'),
       dataType: 'html'
     }).done(function(response){
-      $resp = $(response).children('.auth-container')
+      $resp = $(response).children('.auth-container');
       $target.closest('#content').children('#pop-in').html($resp);
-      debugger
+
     });
 
-    $('#content').css("backgroundColor","#888877")
-
+    $('#content').css("backgroundColor","#888877");
   });
 
   $('#signup-link').on('click', function(event) {
@@ -27,13 +26,23 @@ $(document).ready(function() {
       url: $target.attr('href'),
       dataType: 'html'
     }).done(function(response){
-      $resp = $(response).children('.auth-container')
+      $resp = $(response).children('.auth-container');
       $target.closest('#content').children('#pop-in').html($resp);
-      debugger
     });
 
-    $('#content').css("backgroundColor","#888877")
-
+    $('#content').css("backgroundColor","#888877");
   });
 
+  $('#delete-button').on('submit', function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+
+    $.ajax({
+      type: 'DELETE',
+      url: $target.attr('action')
+    }).done(function(response) {
+      $target.closest('tr').remove();
+    });
+
+  });
 });
