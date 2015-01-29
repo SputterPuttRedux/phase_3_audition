a6803e4a0c89c405f7bdf0f30db650dfee7b496b
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-01-28 21:06:21 -0500

    Fully seed db w/ faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,34 @@ live_action = Genre.create(
 
 baby.genres << live_action
 dionne.movies << baby
+
+genres = ["Live-Action", "Foreign", "Comedy", "Drama", "Political","Science-Fiction", "Fantasy", "Historical"]
+
+10.times do |i|
+  Genre.create(
+    name: genres[i]
+    )
+end
+
+10.times do |i|
+  Movie.create(
+    name: Faker::Lorem.word,
+    description: Faker::Lorem.paragraph,
+    rating: rand(1..5)
+    )
+end
+
+Movie.all.each do |mov|
+  mov.genres << Genre.all.sample(2)
+end
+
+10.times do |i|
+  User.create(
+    name: Faker::Name.name,
+    password: "cat"
+    )
+end
+
+User.all.each do |usr|
+  usr.movies << Movie.all.sample(rand(1..3))
+end
